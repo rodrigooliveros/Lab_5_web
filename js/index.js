7def8148cbde29d677035b8fb79c211e5ba5f105
rodrigooliveros <A01329696@tec.mx>
2023-01-23 21:21:00 -0600

    3 intento

--- js/index.js
+++ js/index.js
@@ -6,14 +6,18 @@ $("#itemsList").on("click", "button.del", deleteItem)
 function addItems(e){
     e.preventDefault()
     let newItem = $("#newText").val()
-    //alert(newItem)
-    $("#itemsList").append(`<div class = "shopItem"> <li class = "lis"> <p class = "itemShop"> ${newItem} </p> </li> <br> <button class = "checar" id = "checar"> Check </button> <button class = "del" id = "del"> Delete </button> <br></div>`)
+    $("#itemsList").append(`<div class = "shopItem">
+    <li class = "lis">
+    <p class = "itemShop" id = "item"> ${(newItem)} </p> </li> <br>
+    <button class = "checar" id = "checar"> Check </button>
+    <button class = "del" id = "del"> Delete </button>
+    </div>`)
 }
 
 function checkItem(e){
     e.preventDefault()
     //alert("hola")
-    $("p").toggleClass('chec')
+    $(this).parent().find("p.itemShop").toggleClass('chec')
 }
 
 function deleteItem(e){
